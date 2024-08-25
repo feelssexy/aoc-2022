a651cdf445f3d481555ab3352c560eee1965555c
user <47542259+feelssexy@users.noreply.github.com>
2024-08-25 03:48:37 +0200

    some short stupidty sesh contributed by urs truly :3

--- C09.nim
+++ C09.nim
@@ -10,7 +10,7 @@ type Dir = enum
 type Move = (Dir, int)
 
 proc dir(x: (Dir, int)): Dir = x[0]
-proc amount(x: (Dir, int)): Dir = x[1]
+proc dist(x: (Dir, int)): int = x[1]
 
 proc drawField() =
   discard
@@ -19,15 +19,37 @@ proc fieldDimensions(moves: seq[Move]): (int, int, int, int) =
   # (origin_x, origin_y, w, h), origin bottom-left
   var pos = (0, 0)
   for m in moves:
+    let i = (if m.dir in {Dir.U, D}c
     case m.dir
     of Dir.U:
+      pos[1] += m.dist
+    of Dir.R:
+      pos[0] += m.dist
+    of Dir.D:
+      result[3] -= m.dist
+      result[1] -= m.dist
+    of Dir.L:
+      result[2] -= m.dist
+      result[0] -= m.dist
+    
+    #[
+    # smh nim isn't smart enough for this
+    # this code is pretty wrong for getting dimensions actually
+    let i = (if m.dir in {Dir.U, D}: 1 else: 0)
+    result[i+2] += x * m.dist
+    if m.dir in {Dir.U, R}:
+      result[i] += x * m.dist
+    ]#
+      
       
 
 let moves: seq[(Dir, int)] = collect:
   for line in input:
     let splt = line.split
     (parseEnum[Dir](splt[0]), splt[1].parseInt)
-    
+
+echo fieldDimensions(moves[0..<3])
+
 block part1:
   var i = 0
   echo moves[0..<5]
